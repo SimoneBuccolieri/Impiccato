b01c8338cc5739a3ee4a530328d65e2edf0aef5e
SimoneBuccolieri <simobucc2nd@gmail.com>
2025-03-11 14:24:10 +0100

    wip

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/simonebuccolieri/CLionProjects/Impiccato
+# Install script for directory: /Users/simonebuccolieri/GitHub/Impiccato
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/simonebuccolieri/CLionProjects/Impiccato/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/simonebuccolieri/GitHub/Impiccato/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- main.cpp
+++ main.cpp
@@ -1,133 +1,156 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
+
+vector<string> draw = {
+    "   +---+\n"
+    "   |   |\n"
+    "       |\n"
+    "       |\n"
+    "       |\n"
+    "       |\n"
+    "=========\n",  // 0 errori
+
+    "   +---+\n"
+    "   |   |\n"
+    "   O   |\n"
+    "       |\n"
+    "       |\n"
+    "       |\n"
+    "=========\n",  // 1 errore
+
+    "   +---+\n"
+    "   |   |\n"
+    "   O   |\n"
+    "   |   |\n"
+    "       |\n"
+    "       |\n"
+    "=========\n",  // 2 errori
+
+    "   +---+\n"
+    "   |   |\n"
+    "   O   |\n"
+    "  /|   |\n"
+    "       |\n"
+    "       |\n"
+    "=========\n",  // 3 errori
+
+    "   +---+\n"
+    "   |   |\n"
+    "   O   |\n"
+    "  /|\\  |\n"
+    "       |\n"
+    "       |\n"
+    "=========\n",  // 4 errori
+
+    "   +---+\n"
+    "   |   |\n"
+    "   O   |\n"
+    "  /|\\  |\n"
+    "  /    |\n"
+    "       |\n"
+    "=========\n",  // 5 errori
+
+    "   +---+\n"
+    "   |   |\n"
+    "   O   |\n"
+    "  /|\\  |\n"
+    "  / \\  |\n"
+    "       |\n"
+    "=========\n"  // 6 errori -> GAME OVER
+};
 
 class Impiccato {
 private:
     string parola;
     string parolaHidden;
-    int errori=0;
+    int errori;
+
 public:
     Impiccato() {
         vector<string> parole;
-        fstream fs("/Users/simonebuccolieri/CLionProjects/Impiccato/cmake-build-debug/Words.txt");
+        fstream fs("words.txt");
 
         if (!fs.is_open()) {
-            cerr << "Errore" << endl;
+            cerr << "Errore: il file 'words.txt' è mancante!\n";
             exit(1);
         }
 
         string word;
         while (getline(fs, word)) {
-            parole.push_back(word);
-        }
-        fs.close();
-        // Scegli parola casuale
-        parola = parole[rand() % parole.size()];
-        parolaHidden = string(parola.length(), 'X');
-    }
-    void check(char s){
-        int guess=0;
-        for (int i=0;i<parola.length();i++) {
-            if (parola[i] == s) {
-                parolaHidden[i]=s;
-                guess=1;
+            if (!word.empty()) {
+                parole.push_back(word);
             }
         }
-        if (guess==0) {
+        fs.close();
+
+        if (parole.empty()) {
+            cerr << "Errore\n";
+            exit(1);
+        }
+
+        // Scegli parola casuale
+        parola = parole[rand() % parole.size()];
+        parolaHidden = string(parola.length(), '_');
+        errori = 0;
+    }
+
+    void check(char s) {
+        bool guess = false;
+        for (size_t i = 0; i < parola.length(); i++) {
+            if (tolower(parola[i]) == tolower(s)) {
+                parolaHidden[i] = parola[i];
+                guess = true;
+            }
+        }
+
+        if (!guess) {
             errori++;
         }
         display();
     }
-    int win() {
-        if (parola==parolaHidden) {
-            cout<<"Hai Vinto!";
-            return 0;
+
+    bool win() {
+        if (parola == parolaHidden) {
+            cout << "\n Hai Vinto! La parola era: " << parola << " \n";
+            return false;
         }
-        if (errori==6) {
-            cout<<"Hai Perso! La parola era "<<parola;
-            return 0;
+        if (errori == 6) {
+            cout << "\n Hai Perso! La parola era: " << parola << " \n";
+            return false;
         }
-        return 1;
+        return true;
     }
+
     void display() {
         #ifdef _WIN32
-                system("cls");
+            system("cls");
         #else
-                system("clear");
+            system("clear");
         #endif
-        cout<<"*-*-*-*-*-*-*-*-*-*-*-*-*"<<endl;
-        cout<<parolaHidden<<endl;
-        vector<string> impiccato = {
-            "   +---+\n"
-            "   |   |\n"
-            "       |\n"
-            "       |\n"
-            "       |\n"
-            "       |\n"
-            "=========\n",  // 0 errori
 
-            "   +---+\n"
-            "   |   |\n"
-            "   O   |\n"
-            "       |\n"
-            "       |\n"
-            "       |\n"
-            "=========\n",  // 1 errore
-
-            "   +---+\n"
-            "   |   |\n"
-            "   O   |\n"
-            "   |   |\n"
-            "       |\n"
-            "       |\n"
-            "=========\n",  // 2 errori
-
-            "   +---+\n"
-            "   |   |\n"
-            "   O   |\n"
-            "  /|   |\n"
-            "       |\n"
-            "       |\n"
-            "=========\n",  // 3 errori
-
-            "   +---+\n"
-            "   |   |\n"
-            "   O   |\n"
-            "  /|\\  |\n"
-            "       |\n"
-            "       |\n"
-            "=========\n",  // 4 errori
-
-            "   +---+\n"
-            "   |   |\n"
-            "   O   |\n"
-            "  /|\\  |\n"
-            "  /    |\n"
-            "       |\n"
-            "=========\n",  // 5 errori
-
-            "   +---+\n"
-            "   |   |\n"
-            "   O   |\n"
-            "  /|\\  |\n"
-            "  / \\  |\n"
-            "       |\n"
-            "=========\n"  // 6 errori -> GAME OVER
-        };
-
-        cout << impiccato[errori] << endl;
-        cout<< "Errori: "<<errori<<"/6"<<endl;
+        cout << "\n*-*-*-*-*-*-*-*-*-*-*-*-*\n";
+        cout << "Parola: " << parolaHidden << "\n";
+        cout << draw[errori] << endl;
+        cout << "Errori: " << errori << "/6\n";
     }
 };
+
 int main() {
     srand(time(0));
     Impiccato game;
-    char word;
+    char letter;
+
     game.display();
     while (game.win()) {
-        cin>>word;
-        game.check(tolower(word));
+        cout << "Inserisci una lettera: ";
+        cin >> letter;
+        game.check(letter);
     }
+    return 0;
 }
